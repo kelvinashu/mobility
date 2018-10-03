1d9178573b4509bad4c3510109ae8091ebcd24b6
Kelvin Ashu <kelvin@pinevalleyinstitute.co.za>
2018-10-03 11:59:32 +0200

    Fixed wrong merge issue

--- za/co/mobility/payments/Payment.java
+++ za/co/mobility/payments/Payment.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 public class Payment {
-
     private int amount;
-=======
-public class Payment{
-private String emplId;
->>>>>>> c2e9a6aaa98e97343237c6210e78e633a185b06e
+    private String emplId;
 }
